edf559679fcc64e04921ba7a1f0e744aa6b6a0f0
Lorenzo Costanzia di Costigliole <lorenzo.costanzia@gmail.com>
2020-08-19 23:16:32 +0200

    Adjust font size to fit

--- app/models/Label.scala
+++ app/models/Label.scala
@@ -1,5 +1,6 @@
 package models
 
 case class Label(line1: String, line2: String, line3: String, line4: String) {
-  def paragraph = List(line1, line2, line3, line4).mkString("\n")
+  val lines: Seq[String] = List(line1, line2, line3, line4)
+  val paragraph: String = lines.mkString("\n")
 }
--- app/services/LabelWriter.scala
+++ app/services/LabelWriter.scala
@@ -3,20 +3,33 @@ package services
 import java.io.File
 
 import com.itextpdf.io.font.constants.StandardFonts
-import com.itextpdf.kernel.font.PdfFontFactory
+import com.itextpdf.kernel.font.{PdfFont, PdfFontFactory}
 import com.itextpdf.kernel.pdf.{PdfDocument, PdfWriter}
 import com.itextpdf.layout.Document
 import com.itextpdf.layout.element.{Paragraph, Table}
 import com.itextpdf.layout.property.UnitValue
 import models.Label
+import play.api.Logging
 
-object LabelWriter {
+object LabelWriter extends Logging {
   // PostScript points per centimetre
   private final val PT_PER_CM = 28.3464566929f
   private final val LABEL_WIDTH = UnitValue.createPointValue(1.7f * PT_PER_CM)
   private final val LABEL_HEIGHT = UnitValue.createPointValue(0.7f * PT_PER_CM)
   private final val LABEL_FONT = StandardFonts.TIMES_ROMAN
-  private final val LABEL_FONT_SIZE =  3f
+  // Height of label minus some padding (1 pt), divided by four lines
+  // This is also used as fixed leading in paragraphs (so lines remain horizontally aligned)
+  private final val LABEL_MAX_FONT_SIZE = LABEL_HEIGHT.getValue / 4f
+
+  private def computeOptimalFontSize(font: PdfFont, text: Label) = {
+    // Get width of longest line
+    // Result is in thousands of user pt
+    val longestGlyphWidth = text.lines.map(font.getWidth).max / 1e3f
+    val fontSize = LABEL_WIDTH.getValue / longestGlyphWidth
+
+    logger.debug(f"fontSize: $fontSize, maxFontSize: $LABEL_MAX_FONT_SIZE")
+    math.min(fontSize, LABEL_MAX_FONT_SIZE)
+  }
 
   def createLabels(content: Seq[Label], dest: File): Unit = {
     // Created font is document specific and cannot be reused
@@ -28,11 +41,11 @@ object LabelWriter {
     doc.add(table)
 
     content.foreach( lbl => {
+      val fontSize = computeOptimalFontSize(font, lbl)
       val para = new Paragraph(lbl.paragraph)
         .setFont(font)
-        .setFontSize(LABEL_FONT_SIZE)
-        .setWidth(LABEL_WIDTH)
-        .setHeight(LABEL_HEIGHT)
+        .setFontSize(fontSize)
+        .setFixedLeading(LABEL_MAX_FONT_SIZE)
       table.addCell(para)
     })
     table.complete()
